18a080186d6c315e82f2346d57549c8fbfe074e7
Fahd Jibril <jibreelfahd@gmail.com>
2025-02-17 23:48:45 +0100

    add new class ProjectList

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-function Autobind (_: any, _2: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+function Autobind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+): PropertyDescriptor {
   const originalDescriptor = descriptor.value;
   const newDescriptor: PropertyDescriptor = {
     configurable: true,
@@ -7,8 +11,71 @@ function Autobind (_: any, _2: string, descriptor: PropertyDescriptor): Property
       const boundFn = originalDescriptor.bind(this);
       return boundFn;
     },
-  }
+  };
   return newDescriptor;
+}
+
+// validate user input
+interface ValidatorConfig {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validators: ValidatorConfig) {
+  let isValid = true;
+
+  if (validators.required) {
+    isValid = validators.value.toString().trim().length !== 0;
+  }
+
+  if (validators.minLength != null && typeof validators.value === "string") {
+    isValid = validators.value.length < validators.minLength;
+  }
+
+  if (validators.maxLength != null && typeof validators.value === "string") {
+    isValid = validators.value.length < validators.maxLength;
+  }
+
+  if (validators.min != null && typeof validators.value === "number") {
+    isValid = validators.value > validators.min;
+  }
+
+  if (validators.max != null && typeof validators.value === "number") {
+    isValid = validators.value < validators.max;
+  }
+
+  return isValid;
+}
+
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  title: string;
+  description: string;
+  people: number;
+
+  constructor(t: string, desc: string, p: number) {
+    this.title = t;
+    this.description = desc;
+    this.people = p;
+
+    this.templateElement = document.querySelector('.project-list')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true)
+
+    this.element = importedNode.firstElementChild! as HTMLElement;
+  }
+
+  renderTemplate() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
 }
 
 class ProjectInput {
@@ -47,13 +114,40 @@ class ProjectInput {
     this.renderTemplate();
   }
 
-  private getUserInput (): [string, string, number] | void {
+  private getUserInput(): [string, string, number] | void {
     const title = this.titleInputElement.value;
     const description = this.descriptionInputElement.value;
     const people = this.peopleInputElement.value;
 
-    if (title.trim().length === 0 || description.trim().length === 0 || people.trim().length === 0) {
-      alert('Invalid input, please try again later!');
+    const titleValidatable: ValidatorConfig = {
+      value: title,
+      required: true,
+      minLength: 2,
+      maxLength: 15,
+    };
+
+    const descValidatable: ValidatorConfig = {
+      value: description,
+      required: true,
+      minLength: 2,
+      maxLength: 15,
+    };
+
+    const peopleValidatable: ValidatorConfig = {
+      value: +people,
+      required: true,
+      min: 1,
+      max: 10,
+    };
+
+    if (
+      validate(titleValidatable) &&
+      validate(descValidatable) &&
+      validate(peopleValidatable)
+    ) {
+      alert("Invalid input, please try again later!");
+      const prjList = new ProjectList(title, description, +people);
+      console.log(prjList);
       return;
     } else {
       return [title, description, +people];
@@ -63,7 +157,7 @@ class ProjectInput {
   @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    
+
     const userInput = this.getUserInput();
 
     if (Array.isArray(userInput)) {
@@ -71,13 +165,12 @@ class ProjectInput {
       this.clearInput();
       console.log(title, description, people);
     }
-
   }
 
   private clearInput() {
-    this.titleInputElement.value = '';
-    this.descriptionInputElement.value = '';
-    this.peopleInputElement.value = '';
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
   }
 
   private configure() {
